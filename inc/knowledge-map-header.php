693d8ab8bb881a430776a526d9962d7e0f72ce6c
pkraker <pkraker@openknowledgemaps.org>
2021-02-05 14:06:15 +0100

    Fix header issues

--- inc/knowledge-map-header.php
+++ inc/knowledge-map-header.php
@@ -3,8 +3,11 @@ include_once dirname(__FILE__) . '../../lib/load-config.php';
 include_once dirname(__FILE__). '../../lib/get-params.php';
 include_once dirname(__FILE__). '../../conf/config.php';
 
+$ini_array = loadConfigFile();
+$headstart_path = loadConfigOption($ini_array, "headstart_path", "general");
+
 $id_get = getParam("id", INPUT_GET, FILTER_SANITIZE_STRING, true);
-$id = ($id_get !== false && $id_get !== "") ? ($id_get) : ($search_flow_config["default_id"]);
+$id = ($id_get !== false && $id_get !== "") ? ($id_get) : ($search_flow_config["vis_default_id"]);
 
 $protocol_server = getParam("HTTPS", INPUT_SERVER, FILTER_SANITIZE_STRING, true, true);
 $protocol = $protocol_server !== false && $protocol_server !== 'off' ? 'https:' : 'http:';
@@ -18,10 +21,10 @@ $has_custom_title = ($custom_title !== false)?(true):(false);
 
 $is_embed = getParam("embed", INPUT_GET, FILTER_VALIDATE_BOOLEAN, true);
 
-$context_json = curl_get_contents($protocol . $HEADSTART_URL . "server/services/getContext.php?vis_id=$id");
+$context_json = curl_get_contents($protocol . $headstart_path . "server/services/getContext.php?vis_id=$id");
 $context = json_decode($context_json);
 
-$query = (isset($context) && $context->query !== null) ? ($context->query) : (search_flow_config["default_id"]);
+$query = (isset($context) && $context->query !== null) ? ($context->query) : ($search_flow_config["vis_default_id"]);
 $query = preg_replace("/\\\\\"/", "&quot;", $query);
 $query = preg_replace("/\\\'/", "&apos;", $query);
 
