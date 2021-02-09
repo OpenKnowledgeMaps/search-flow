109591ffc03e546c68cc71d5cac516edc8a40efe
pkraker <pkraker@openknowledgemaps.org>
2021-02-09 21:50:32 +0100

    Make browser warning configurable

--- conf/config.php
+++ conf/config.php
@@ -226,6 +226,9 @@ if(isset($search_flow_config_local)) {
 
             }
         }
+        , banner_texts: {
+            browser_unsupported_warning: 'You are using <strong>an unsupported browser</strong>. This website was successfully tested with the latest versions of <strong>Firefox, Chrome, Safari, Opera, and Edge</strong>. We strongly suggest <strong>to switch to one of the supported browsers.</strong>' 
+        }
     }
     
     if(typeof search_flow_config_local !== "undefined") {
