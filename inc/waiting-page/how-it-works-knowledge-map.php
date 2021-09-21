0fb43b6794ff4af3464a4b8d9be2657ed5c51f6c
tanteuschi <maxi@openknowledgemaps.org>
2021-09-21 15:38:39 +0200

    added images and CSS changes to how it works

--- inc/waiting-page/how-it-works-knowledge-map.php
+++ inc/waiting-page/how-it-works-knowledge-map.php
@@ -7,9 +7,9 @@
 
             <p>
                 <span class="number">1</span>
-                <img class="howimg" src="./img/howitworks/top100.png" alt=''>
+                <img class="howimg" src="https://openknowledgemaps.org/img/howitworks/top100.png" alt=''>
             </p> 
-            <p>A Knowledge Map presents you with a topical overview for your search query based on the 100 <a class="underline" href="./faq">most relevant</a> documents matching your query.
+            <p>A Knowledge Map presents you with a topical overview for your search query based on the 100 <a class="underline" href="https://openknowledgemaps.org/faq" target="_blank">most relevant</a> documents matching your query.
             </p>
 
         </div>
@@ -18,7 +18,7 @@
 
             <p>
                 <span class="number">2</span>
-                <img class="howimg" src="./img/howitworks/textsimilarity.png" alt=''>
+                <img class="howimg" src="https://openknowledgemaps.org/img/howitworks/textsimilarity.png" alt=''>
             </p>
             <p>We use text similarity to create the knowledge maps.
                 The algorithm groups those documents together that have many words in common.
@@ -30,7 +30,7 @@
 
             <p>
                 <span class="number">3</span>
-                <img class="howimg" src="./img/howitworks/headstart-search.png" alt=''>
+                <img class="howimg" src="https://openknowledgemaps.org/img/howitworks/headstart-search.png" alt=''>
             </p>
             <p>The visualization is intended to give you a head start on your 
                 scholarly search. You can identify relevant areas at a glance and documents related to them.
@@ -39,4 +39,11 @@
         </div>
 
     </div>
+    
+    <p class="try-now" style="text-align: center; margin:30px 0 0;">
+        <a class="donate-now" class="basic-button" target="_blank" href="https://openknowledgemaps.org/faq">
+            Find out more in our FAQs
+        </a>
+    </p>
+    
 </div>
