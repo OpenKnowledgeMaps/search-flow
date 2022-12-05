7401192a0a358f65520f75106acdc652f9d828f3
chreman <web@christopherkittel.eu>
2022-12-05 09:42:34 +0100

    add additional import

--- entrypoint.php
+++ entrypoint.php
@@ -10,6 +10,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/search_box.css">
+
+    <script type="module" src="js/searchbox/runner.js"></script>
 </head>
 
 <body class="iframed">
