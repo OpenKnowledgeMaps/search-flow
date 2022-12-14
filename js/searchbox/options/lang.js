8c12ff6ffadd79377ab6c5335785604e3b35fcf6
Alexandra Shubenko <as@belichberg.com>
2022-12-14 14:46:00 +0100

    fax browsers problem, refactor sort lang array

--- js/searchbox/options/lang.js
+++ js/searchbox/options/lang.js
@@ -1,9 +1,16 @@
 "use strict";
 
-import LangOptionsData from '../options/lang_options.json' assert { type: "json" };
+import {LangOptions} from './lang_options.js';
 
+const LANG_OPTIONS = LangOptions.map((item)=>(item.label && item.id) && item)
+    .sort((a, b) => (a.label > b.label) ? 1 : -1)
 
-const LANG_OPTIONS = LangOptionsData.map((item)=>(item.label && item.id) && item).sort((a, b) => a.name > b.name ? -1 : 1)
-
+// put All Languages on the start of array
+LANG_OPTIONS.map((item) => {
+    if (item.id==="all-lang"){
+        const index = LANG_OPTIONS.indexOf(item);
+        LANG_OPTIONS.splice(0, 0, LANG_OPTIONS.splice(index, 1)[0]);
+    }
+})
 
 export default LANG_OPTIONS;
