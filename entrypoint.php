bd17bd1843f8f155b1e4329e2e57225104b23d26
Alexandra Shubenko <as@belichberg.com>
2023-04-12 15:03:12 +0200

    add meta tag to entrypoint.php to fix mobile scaling

--- entrypoint.php
+++ entrypoint.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
     <meta name="referrer" content="origin-when-cross-origin"/>
 
