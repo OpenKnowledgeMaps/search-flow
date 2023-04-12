2176fdec6092582f56daff2406f69c7789c7e187
Alexandra Shubenko <as@belichberg.com>
2023-04-12 15:00:24 +0200

    add meta tag to entrypoint.php to fix mobile scaling

--- entrypoint.php
+++ entrypoint.php
@@ -3,6 +3,9 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
+    <meta name="referrer" content="origin-when-cross-origin"/>
 
     <title>Open Knowledge Maps Search Box</title>
 
@@ -13,9 +16,6 @@
     <link rel="stylesheet" href="search-flow/css/search_box.css">
 
     <script type="module" src="search-flow/js/searchbox/runner.js"></script>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
-    <meta name="referrer" content="origin-when-cross-origin"/>
 </head>
 
 <body class="iframed">
