5f6b95df972478f556f7e422b797acf9cad8d98a
Alexandra Shubenko <as@belichberg.com>
2023-04-12 14:59:56 +0200

    add meta tag to entrypoint.php to fix mobile scaling

--- inc/search-form/new-search-form.php
+++ inc/search-form/new-search-form.php
@@ -18,6 +18,3 @@
 </script>
 
 <script type="module" src="search-flow/js/searchbox/runner.js"></script>
-<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
-<meta name="referrer" content="origin-when-cross-origin"/>
