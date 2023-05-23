ad74005b37fa9914c07b8cd34039bf5a1dbf8d40
Alexandra Shubenko <as@belichberg.com>
2023-05-23 16:09:09 +0200

    small interface fixes

--- js/searchbox/components/DataSource.js
+++ js/searchbox/components/DataSource.js
@@ -7,13 +7,15 @@ import SERVICES_OPTIONS from "../options/services.js";
 const e = React.createElement;
 
 const DataSource = ({value, setValue}) => {
-  return e(RadioInputList, {
-    label: "Select a data source",
-    options: SERVICES_OPTIONS,
-    name: "service",
-    value: value,
-    setValue: setValue,
-  });
+    return e("div", {className: "datasource-style"},
+        e(RadioInputList, {
+            label: "Select a data source",
+            options: SERVICES_OPTIONS,
+            name: "service",
+            value: value,
+            setValue: setValue,
+        })
+    )
 };
 
 export default DataSource;
