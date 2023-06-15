1d2a81d354ef3ef534542f7fa56bcdd8ecb0c5ff
Christopher Kittel <web@christopherkittel.eu>
2023-06-15 20:36:07 +0200

    Update header-main.php

--- php/header-main.php
+++ php/header-main.php
@@ -1,6 +1,6 @@
 <!-- Requires header-head.php -->
 
-<link rel="stylesheet" href="<?php echo $searchflow_path ?>css/search-flow.css?v=2023-06-07">
+<link rel="stylesheet" href="<?php echo $searchflow_path ?>css/search-flow.css?v=2023-06-15">
 <script type="text/javascript" src="<?= $searchflow_path ?>lib/browser-detect.js"></script>
 <script>
     var browser = BrowserDetect.browser;
