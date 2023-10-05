865ea9d213bfe572ac4cf221c6297d198c45d908
Alexandra Shubenko <as@belichberg.com>
2023-10-05 11:09:23 +0200

    set custom_title param from context

--- inc/visualization/visualization-header.php
+++ inc/visualization/visualization-header.php
@@ -42,6 +42,11 @@ if($search_flow_config["vis_load_context"]) {
         $service = "plos";
     }
 
+//    set parameter $custom_title to true if $context["custom_title"] is not null
+    $has_custom_title = $context["custom_title"] !== null;
+//    set parameter $custom_title to $context["custom_title"] if $context["custom_title"] is not null
+    $custom_title = setVariableFromContext($context, "custom_title");
+
     $query = setVariableFromContext($context
                                         , "query"
                                         , $search_flow_config["enable_default_query"]
