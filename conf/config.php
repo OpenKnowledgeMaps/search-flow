a275304b7b12917a1b60279323751dae7a6a47c8
chreman <web@christopherkittel.eu>
2024-07-30 15:59:29 +0200

    error message text updates for orcid

--- conf/config.php
+++ conf/config.php
@@ -357,18 +357,18 @@ if(isset($search_flow_config_local)) {
             },
             orcid_no_results_error: {
                 title: 'Sorry! We could not create a <span class="vis_type_name">knowledge map</span>.'
-                , reason: 'There are no documents available for ???.'
-                , remedy: 'Have another try!'
+                , reason: 'There are no documents available for %orcid%.'
+                , remedy: 'Please try again with another ORCiD.'
                 , contact: 'For more information about our service please <a class="underline" href="https://openknowledgemaps.org/faq" target="_blank">see our FAQs</a>. If you think that there is something wrong with our service, please let us know at <a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
                 , resolution_type: "link"
                 , resolution_label: "Refresh this page"
                 , resolution_link: "javascript:location.reload()"
 
             },
-            orcid_no_results_error: {
-                title: "ORCID no results error - Sorry! We could not create a knowledge map."
-                , reason: 'There are no documents available for %orcid%.'
-                , remedy: 'Please try again with another ORCiD ID.'
+            invalid_orcid_id_error: {
+                title: 'Sorry! We could not create a <span class="vis_type_name">knowledge map</span>.'
+                , reason: 'Most likely you have entered the wrong ORCiD.'
+                , remedy: 'Please verify the ORCiD and try again.'
                 , contact: 'For more information about our service please <a class="underline" href="https://openknowledgemaps.org/faq" target="_blank">see our FAQs</a>. If you think that there is something wrong with our service, please let us know at <a class="underline" href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>'
                 , resolution_type: "link"
                 , resolution_label: "Refresh this page"
